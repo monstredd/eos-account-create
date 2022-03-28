2ed15b9d31810af1a7e7484ad49aea563d098953
monstredd <92199704+monstredd@users.noreply.github.com>
2022-03-28 16:00:13 +0800

    Update transit_service.pl
    fix bug

--- transit_service/transit_service.pl
+++ transit_service/transit_service.pl
@@ -1,14 +1,14 @@
 use strict;
 use warnings;
 
-my $wallet_name = "";  # keosd wallet name  钱包名称
-my $pwd_file = "";     # password file      钱包密码文件名称
-my $bridge_account = "";#bridge accounts    中转账号
+my $wallet_name = "";    #钱包名称
+my $pwd_file = "";       #钱包密码文件名称
+my $bridge_account = ""; #中转账号
 
-my $pos =  $ARGV[0];     #启动值要选择最近已经确认的区块，并且已经账号注册完成的编号。
+my $pos =  $ARGV[0];     #启动值要选择最近已经确认的区块中没有注册完成的编号。
 my $offset = $ARGV[1];   #填1 返回一条记录。
 
-$pos++;
+#$pos++;
 
 while()
 {
@@ -27,7 +27,7 @@ print "**********\n";
  if( $get_actions[0] =~ /\?\D+(\d+).+eosio\.token::transfer/ )  #如果碰到是正在打包等待完全确认的。
  {
     print "$pos unconfirmed\n";
-    sleep (60);
+    sleep (10);
     redo;
  }
  elsif($get_actions[0] =~ /#\D+(\d+).+eosio\.token::transfer/)  #如果碰到的是已经确认的。
@@ -55,15 +55,21 @@ print "**********\n";
 	    `cat $pwd_file |cleos wallet unlock -n $wallet_name`;
          while()
          {
-    	  my @reg_back = `cleos transfer $bridge_account accountspapa "$send EOS" "$memo" 2>&1`;  #注册
+    	  my @reg_back = `cleos transfer $bridge_account accountspapa "$send EOS" "$memo" -x 180  2>&1`;  #注册
           if ($reg_back[0] =~ /Error\s+3080004|3080002|3081001/ ) #cpu或者net资源不足错误,或者deadline
           {
             print "reg back first line : $reg_back[0] \n";
             print "power up the $bridge_account\n";
-            my @powerup_back =`cleos push action eosio powerup '["$bridge_account","$bridge_account","1","111460","267858576","0.0090 EOS"]' -p $bridge_account\@active 2>&1`;
+            my @powerup_back =`cleos push action eosio powerup '["$bridge_account","$bridge_account","1","111460","535717152","0.0100 EOS"]' -p $bridge_account\@active 2>&1`;
             print "power up result: \n";
             print @powerup_back;
 	    sleep(10);
+          }
+          elsif($reg_back[0] =~ /Error\s+3080006/ ) #  Transaction took too long
+          {
+            print "reg back: \n";
+            print @reg_back;
+            sleep(10);
           }
           elsif($reg_back[0] =~ /Error\s+3050001/) #账号已经被注册，则使用随机号注册,如果随机账号也被注册，不会跳出循环，会继续尝试生成新的，直到注册成功。
           {
@@ -72,6 +78,12 @@ print "**********\n";
            $memo =~ s/^.{12}/$random_account/ ;
            print "random : $memo \n";
            sleep(10);
+          }
+	  elsif($reg_back[0] =~ /Error/ ) #  其他错误，重新尝试注册
+          {
+            print "reg back: \n";
+            print @reg_back;
+            sleep(10);
           }
           else
           {
@@ -113,10 +125,21 @@ sub process_memo
 {
   my $memo_return;
   my $r_account;
-  if ($_[0] =~ /^\s*(\w+-EOS\w+)\s*/) #标准模式 账号名-EOS前缀公钥
+  if ($_[0] =~ /^\s*(\w+)-(EOS\w+)\s*/) #标准模式 账号名-EOS前缀公钥
   { 
     print "moudle 1\n";
-    return $1; #返回去掉前后空格 
+    my $temp_string = $1;
+    $temp_string =~ tr/A-Z/a-z/;
+    # print length($temp_string);
+    if (length($temp_string) != 12)
+    {
+    $temp_string = random_account_generation();
+    }
+    elsif($temp_string =~/0|6|7|8|9/)
+    {
+    $temp_string = random_account_generation();
+    }
+    return "$temp_string-$2"; #返回去掉前后空格 
   }
   elsif ($_[0] =~ /^\s*(EOS.+)\s*/) # EOS前缀公钥
   {
@@ -128,10 +151,24 @@ sub process_memo
   elsif($_[0] =~ /^\s*(\w+)-(\w+)\s*/) #  账号名-无EOS前缀公钥
   {
    print "moudle 3\n";
-   $memo_return = "$1-EOS$2";
+    my $temp_string = $1;
+    my $temp_pubkey = $2;
+    $temp_string =~ tr/A-Z/a-z/;
+    # print length($temp_string);
+    if (length($temp_string) != 12)
+    {       
+    $temp_string = random_account_generation();
+    }
+    elsif($temp_string =~/0|6|7|8|9/)
+    {
+    $temp_string = random_account_generation();
+    }
+
+   $memo_return = "$temp_string-EOS$temp_pubkey";
+
    return $memo_return; 
   }
-  elsif($_[0] !~ /^\s*EOS/ and $_[0] =~ /\s*(\w{50})\s*/) # 无前缀公钥
+  elsif($_[0] !~ /^\s*EOS/ and $_[0] =~ /^\s*(\w{50})\s*$/) # 无前缀公钥
   {
    print "moudle 4\n";
    $r_account = random_account_generation();
@@ -153,4 +190,5 @@ sub random_account_generation
  my @char_array = ('a'..'z',1..5);
  my $r_account = join '', map{ $char_array[int rand @char_array] } 0..11;
  return $r_account;
+}
 
